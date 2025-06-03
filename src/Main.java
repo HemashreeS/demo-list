1fa1ad35427716e979a0d89e50051fa9a8b72c17
HemashreeS <hemacbe01@gmail.com>
2025-06-03 15:53:14 +0530

    LinkedList demo

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.push(5);
+        singlyLinkedList.size;
+        singlyLinkedList.head.value;
+        singlyLinkedList.tail.value;
+
+        singlyLinkedList.push(10);    // Success
+//        singlyLinkedList.size;     // 2
+//        singlyLinkedList.head.value;    // 5
+//        singlyLinkedList.head.next.value;  // 10
+//        singlyLinkedList.tail.value;    // 10
+
+    }
+}
---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,29 @@
+public class SinglyLinkedList {
+    //TODO
+    public Node head;
+    public Node tail;
+    public int size;
+
+    public Node insertSinglyListedList(int nodeValue) {
+        head = new Node();
+        Node node = new Node();
+        node.next = null;
+        node.value = nodeValue;
+        head = tail = node;
+        size = 1;
+        return head;
+    }
+
+    public void push(int nodeValue) {
+        if(head == null) {
+            insertSinglyListedList(nodeValue);
+            return;
+        } else {
+            Node newNode = new Node();
+            newNode.value = nodeValue;
+            newNode.next = null;
+            tail.next = newNode;
+            size++;
+        }
+    }
+}
